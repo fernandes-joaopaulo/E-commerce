2186ee0e1bc69b3ce1c523f4d98b19ae0dd8ff8b
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-01-25 23:28:06 -0300

    Cria classes iniciais

---
+++ Ecommerce/src/main/java/com/mycompany/ecommerce/Ecommerce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecommerce;
+
+/**
+ *
+ * @author Joao Paulo
+ */
+public class Ecommerce {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
